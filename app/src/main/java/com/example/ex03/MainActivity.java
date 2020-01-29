f6ea48a78f6d4243d339e4d8499f05c449896c98
OriOfek <aa6055@bs.amalnet.k12.il>
2020-01-29 09:39:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ex03/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.ex03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    int i;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.btn);
+    }
+
+    public void change(View view) {
+        if (i<6)
+        {
+            btn.setText("This is a click number:"+""+i);
+            i++;
+        }
+        else
+        {
+            btn.setText("Enough to click. Go to new start!");
+            i=0;
+        }
+    }
+}
